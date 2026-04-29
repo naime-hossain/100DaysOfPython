1623abf5f9b140f11c210b41c9a6e39b99442b5c
naime-hossain <hossain.naime@yahoo.com>
2026-04-29 23:14:17 +0600

    Day1 final project done

--- 01day/main.py
+++ 01day/main.py
@@ -5,6 +5,17 @@
 # fullName(input("Enter Your First Name : "),input("Enter Your Last Name : "))  
 # print("Hello " + input("What is your name ? : ")  +"! ")
 # print("Your Name Consist of  " + str(len(input("What is you Name? : "))))
-name = input("What is your name ? : ");
-length=len(name)
-print(length)
+# name = input("What is your name ? : ");
+# length=len(name)
+# print(length)
+
+# Final Project 
+# Brand name generator : take two inputs from user 
+# input 1 : name of your city and input 2 : name of your city
+
+print("welcome to brand name generator \n " )
+city_name = input("What is your city name ? : \n ")
+pet_name = input("what is your pet name ? : \n ")
+brand_name = city_name + " " + pet_name
+print("Your brand name is : " +  brand_name )
+
