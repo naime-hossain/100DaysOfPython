e400b2db8b724e1958776ff2515378c7a50ae43a
naime-hossain <hossain.naime@yahoo.com>
2026-04-29 17:22:06 +0600

    update main.py

--- 01day/main.py
+++ 01day/main.py
@@ -1,6 +1,7 @@
-print("Hello World! \n Welcome to  Redemption of my Life")
-print("Hello Naime" + " " + " Hossain Bhuiyan")
-def fullName(firstname, lastname):
-     print(firstname + " " + lastname)
-fullName(input("Enter Your First Name : "),input("Enter Your Last Name : "))  
-print ("Hello " + input("What is your name ? : ")  +"! ")
+# print("Hello World! \n Welcome to  Redemption of my Life")
+# print("Hello Naime" + " " + " Hossain Bhuiyan")
+# def fullName(firstname, lastname):
+#      print(firstname + " " + lastname)
+# fullName(input("Enter Your First Name : "),input("Enter Your Last Name : "))  
+# print("Hello " + input("What is your name ? : ")  +"! ")
+print("Your Name Consist of  " + str(len(input("What is you Name? : "))))
