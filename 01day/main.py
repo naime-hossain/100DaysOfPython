12f365e4823afbf67290cc461dc66bd2a6ea9fa8
naime-hossain <hossain.naime@yahoo.com>
2026-04-29 17:11:10 +0600

    Day1 of 100 days of Python ongooing

---
+++ 01day/main.py
@@ -0,0 +1,6 @@
+print("Hello World! \n Welcome to  Redemption of my Life")
+print("Hello Naime" + " " + " Hossain Bhuiyan")
+def fullName(firstname, lastname):
+     print(firstname + " " + lastname)
+fullName(input("Enter Your First Name : "),input("Enter Your Last Name : "))  
+print ("Hello " + input("What is your name ? : ")  +"! ")
