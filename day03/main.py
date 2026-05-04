197f21ab06539a5ea6d03db3b2c19c6394473e16
naime-hossain <hossain.naime@yahoo.com>
2026-05-04 09:23:09 +0600

    Day 3 : Rider Height Calculator if,elseIf, comparision operator

---
+++ day03/main.py
@@ -0,0 +1,10 @@
+# Rider height Calculator
+print("Welcome to Rider height checker \n")
+height=float(input("What is your Height in CM ? : cm \n "))
+if height >=120 :
+    print("You can Ride the roller coaster")
+elif height <120 :
+    print("Sorry you cant Ride the roller coaster")
+
+    
+       
