07153f80e42a7232dfe547b60e33613fd36f3bd2
naime-hossain <hossain.naime@yahoo.com>
2026-05-04 09:26:43 +0600

    Day 3 : Odd even checker done

--- day03/main.py
+++ day03/main.py
@@ -1,10 +1,21 @@
 # Rider height Calculator
-print("Welcome to Rider height checker \n")
-height=float(input("What is your Height in CM ? : cm \n "))
-if height >=120 :
-    print("You can Ride the roller coaster")
-elif height <120 :
-    print("Sorry you cant Ride the roller coaster")
 
-    
+
+# print("Welcome to Rider height checker \n")
+# height=float(input("What is your Height in CM ? : cm \n "))
+# if height >=120 :
+#     print("You can Ride the roller coaster")
+# elif height <120 :
+#     print("Sorry you cant Ride the roller coaster")
+
+# Odd pr even checker
+
+print("Welcome to odd or even checker \n")
+number=float(input("Write the number you want to check ? : \n"))
+
+if number%2==0 : 
+    print("This number is even")
+else : 
+    print("This number is odd")
+
        
