65be50a26e371d7314d88944d25fd59ca6a2e4e1
naime-hossain <hossain.naime@yahoo.com>
2026-05-07 16:17:45 +0600

    day 03 done

--- day03/main.py
+++ day03/main.py
@@ -10,12 +10,27 @@
 
 # Odd pr even checker
 
-print("Welcome to odd or even checker \n")
-number=float(input("Write the number you want to check ? : \n"))
+# print("Welcome to odd or even checker \n")
+# number=float(input("Write the number you want to check ? : \n"))
 
-if number%2==0 : 
-    print("This number is even")
+# if number%2==0 : 
+#     print("This number is even")
+# else : 
+#     print("This number is odd")
+
+
+# Roller coaster with age and height checker
+print("Welcome to Roller Coaster Ride \n")
+height=float(input("what is your height in cm ? : \n"))
+if height >=120 :
+    print("You can Ride the roller coaster \n")
+    age=int(input("What is your age ? : \n"))
+    if age <12 :
+        print("please Pay $5")
+    elif age>=12 and age <=18 :
+        print ("Please Pay $7" )
+    else : 
+        print("Please Pay $12")
 else : 
-    print("This number is odd")
-
+    print("Sorry you cant Ride the roller coaster")            
        
