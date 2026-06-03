525ff1529f0a53d4a2c928c06897fb75ff738174
naime-hossain <hossain.naime@yahoo.com>
2026-06-03 16:46:24 +0600

    password generator done

--- Day05/pypassword.py
+++ Day05/pypassword.py
@@ -4,7 +4,7 @@ alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+'] 
 print("welcome to password generator app")
-number_of_letters=int(input("How many letters would you like in your passowrd ?"))
+number_of_letters=int(input("How many characters would you like in your passowrd ?"))
 number_of_numbers=int(input("How many numbers would you like in your passowrd ?"))
 number_of_symbols=int(input("How many symbols would you like in your passowrd ?"))
 random_alphabets = random.sample(alphabets, (number_of_letters-number_of_numbers)-number_of_symbols)
